57a38444de1b2ee32bfaf6b795bf2bf1fca015a7
fzry18 <fazrysuhada168@gmail.com>
2024-06-08 00:26:16 +0800

    adding filepath and publication

--- app/src/main/java/com/sellucose/sellucosebook/data/EpubBook.kt
+++ app/src/main/java/com/sellucose/sellucosebook/data/EpubBook.kt
@@ -1,10 +1,13 @@
 package com.sellucose.sellucosebook.data
 
 import android.graphics.Bitmap
+import org.readium.r2.shared.publication.Publication
 
+// Modifikasi kelas EpubBook untuk menambahkan properti publication
 data class EpubBook(
     val title: String,
     val author: String,
-    val cover: Bitmap?
-// Tambahkan properti ini
+    val cover: Bitmap?,
+    val filePath: String?, // Tambahkan field ini
+    val publication: Publication? // Tambahkan field ini
 )
